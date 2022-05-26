9752d64871eff9496190f32d8724fc17d61e9b3d
roniceyemeli <roniceyemeli@gmail.com>
2022-05-27 00:29:19 +0100

    querying info

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,28 @@
-const {dummyData, UserList} = require('../dummyData')
+const {dummyData, UserList, MovieList} = require('../dummyData');
+const _ = require("lodash");
 
 const resolvers = {
     Query: {
         users(){
             return UserList;
+        },
+        user: (parent, args) => {
+            const id = args.id;
+            const user = _.find(UserList, {id: Number(id)})
+            return user;
+        },
+        movies: () =>{
+            return MovieList
+        },
+        movie: (parent, args) =>{
+            const name = args.name;
+            const movie = _.find(MovieList, {name})
+            return movie;
+        }
+    },
+    User: {
+        favoriteMovies: () =>{
+            return  _.filter(MovieList, (movie) => movie.yearOfPublication >=2000 && movie.yearOfPublication <= 2010)
         }
     }
 }
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -6,12 +6,32 @@ const typeDefs = gql`
 		name: String!
 		username: String!
 		age: Int!
-		nationality: String!
+		nationality: Nationality!
+		friends: [User]
+		favoriteMovies: [Movie]
+	}
+
+	type Movie {
+		id: ID!
+		name: String!
+		yearOfPublication: Int!
+		isInTheaters: Boolean!
 	}
 
 	type Query {
 		users: [User!]!
+		user(id: ID!): User!
+		movies: [Movie!]!
+		movie(name: String!): Movie!
+	}
+
+	enum Nationality {
+		CANADA
+		BRAZIL
+		INDIA
+		GERMANY
+		CHILE
 	}
 `;
 
-module.exports = {typeDefs}
+module.exports = { typeDefs };
